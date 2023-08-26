cbae04b88bc2323b9b0c66c28cbad8404b7b1ce3
fernandez123314 <gonzalesmango498@gmail.com>
2023-08-26 01:19:32 -0300

    Ejercicios Practicos de Operadores y Precedencias(ADICION Y STRINGS)

---
+++ Practica-de-Operadores.js
@@ -0,0 +1,17 @@
+//EJERCICIO 1
+let name = "ADHEMAR";
+let apellido = name + " FERNADNEZ";
+console.log(apellido);
+
+//EJERCICIO 2
+var numero = 10;
+var otro = numero * "10";
+console.log(otro);
+
+//EJERCICIO 3
+var nombre = "ADHEMAR";
+var apellido ="FERNANDEZ";
+nombre = nombre + " JUNIOR";
+var otroApellido = nombre + apellido + " GONZALES";
+
+console.log(otroApellido);
